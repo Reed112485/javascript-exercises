9e3a6487472ba76189354bf1488c328f2fa2d87c
Reed112485 <139658839+Reed112485@users.noreply.github.com>
2024-05-07 11:27:02 -0700

    TDD exercises worked on

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const charArray = word.split("");
+    charArray.reverse();
+    let reversed = charArray.join("");
+    return reversed;
 };
 
 // Do not edit below this line
