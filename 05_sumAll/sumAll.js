dc64c6c93891da1558379e96de21da7b7b3c5f79
Reed112485 <139658839+Reed112485@users.noreply.github.com>
2024-09-24 11:37:22 -0700

    Complete exercises
    Completed javascript exercises 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (num1 > num2) {
+        let ph = num1;
+        num1 = num2;
+        num2 = ph;
+    } else if (num1 < 0 || num2 < 0) {
+        console.log("ERROR");
+        return "ERROR";
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        console.log("ERROR");
+        return "ERROR";
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    console.log(sum);
+    return(sum);
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let conversion = ((fTemp - 32) * (5/9));
+
+  let fixed = parseFloat(conversion.toFixed(1));
+
+  return fixed;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let conversion = (cTemp * 9/5 + 32);
+
+  let fixed = parseFloat(conversion.toFixed(1));
+
+  return fixed;
 };
 
 // Do not edit below this line
